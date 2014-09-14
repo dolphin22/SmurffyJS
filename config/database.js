2830c81e648d5fc1f26f0fadda2925e8bcd96835
Dolphin <nhancaheo.me@gmail.com>
2014-09-14 15:19:57 +0700

    Misc. updates.

--- config/database.js
+++
@@ -1,3 +0,0 @@
-modules.export = {
-	url: "mongodb://smurffy:amo5elU5Q3pjOG1KcFkrOVR1czVRaUZ5dVFwdzFOMFdEdWpXb1JiMHJxZz0K@172.17.0.2:27017/smurffy-production"
-}
--- server.js
+++ server.js
@@ -25,7 +25,6 @@ app.get('/', function(req, res) {
 // Create our Express router
 var router = express.Router()
 
-//mongoose.connect('mongodb://smurffy:amo5elU5Q3pjOG1KcFkrOVR1czVRaUZ5dVFwdzFOMFdEdWpXb1JiMHJxZz0K@172.17.0.2:27017/smurffy-production')
 mongoose.connect(process.env.MONGO_URL)
 
 // Initial dummy router for testing
